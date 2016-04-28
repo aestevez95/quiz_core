660043d877979aeef60f108d5e595e68f1465c20
aestevez95 <antonio.estevez.rodriguez18@gmail.com>
2016-04-28 20:15:53 +0200

    Corregido error tema 8

--- models/index.js
+++ models/index.js
@@ -1,11 +1,23 @@
 var path = require('path');
 
-// Cargar modelo ORM
+// Cargar ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite
-var sequelize = new Sequelize("sqlite:///", 
-			{ storage: "quiz.sqlite" });
+// Usar BBDD SQLite:
+//	DATABASE_URL = sqlite:///
+//	DATABASE_STORAGE = quiz.sqlite
+
+var url, storage;
+
+if (!process.env.DATABASE_URL) {
+	url = "sqlite:///";
+	storage = "quiz.sqlite";
+} else {
+	url = process.env.DATABASE_URL;
+	storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, { storage: storage, omitNull: true });
 
 // Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
@@ -14,12 +26,10 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 sequelize
 .sync()
 .then(function() { // sync() crea la tabla quiz
-  return
     Quiz
     .count()
     .then(function(c) {
       if (c === 0) {       // la tabla se inicializa si está vacía
-        return
 	  Quiz
 	  .create({ question: 'Capital de Italia', answer: 'Roma' })
 	  .then(function() {
