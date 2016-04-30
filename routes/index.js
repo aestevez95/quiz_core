10ffc902b22591189bf55c0823cb5bd13c81ed9a
aestevez95 <antonio.estevez.rodriguez18@gmail.com>
2016-04-30 12:14:50 +0200

    Tema 12: Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,10 @@ router.param('quizId', quizController.load); // autoload :quizId
 router.get('/quizzes', 				quizController.index);
 router.get('/quizzes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', 	quizController.check);
+router.get('/quizzes/new', 			quizController.new);
+router.post('/quizzes', 			quizController.create);
+
+//Definición de ruta de author
 router.get('/author', 				quizController.author);
 
 module.exports = router;
