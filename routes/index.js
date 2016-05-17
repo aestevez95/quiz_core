d3dcb4e554aa487cfa93a1570d0906fbd7d3723f
aestevez95 <antonio.estevez.rodriguez18@gmail.com>
2016-05-18 01:06:02 +0200

    Tema 23: Autorización

--- routes/index.js
+++ routes/index.js
@@ -25,22 +25,22 @@ router.get('/users', 				userController.index); // listado usuarios
 router.get('/users/:userId(\\d+)', 		userController.show); // ver un usuario
 router.get('/users/new', 			userController.new); // formulario sign in
 router.post('/users', 				userController.create); // registrar usuario
-router.get('/users/:userId(\\d+)/edit', 	userController.edit); // editar información de cuenta
-router.put('/users/:userId(\\d+)', 		userController.update); // actualizar información de cuenta
-router.delete('/users/:userId(\\d+)', 		userController.destroy); // borrar cuenta
+router.get('/users/:userId(\\d+)/edit', 	sessionController.loginRequired, userController.edit); // editar información de cuenta
+router.put('/users/:userId(\\d+)', 		sessionController.loginRequired, userController.update); // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)', 		sessionController.loginRequired, userController.destroy); // borrar cuenta
 
 // Definición de rutas de /quizzes 
 router.get('/quizzes', 				quizController.index); // Servir página con listado de preguntas y atender a peticiones de búsqueda
 router.get('/quizzes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', 	quizController.check);
-router.get('/quizzes/new', 			quizController.new);
-router.post('/quizzes', 			quizController.create); // Crear nuevo quiz (post)
-router.get('/quizzes/:quizId(\\d+)/edit', 	quizController.edit);
-router.put('/quizzes/:quizId(\\d+)', 		quizController.update); // Actualizar un quiz (put)
-router.delete('/quizzes/:quizId(\\d+)', 	quizController.destroy); // Borrar un quiz (delete)
+router.get('/quizzes/new', 			sessionController.loginRequired, quizController.new);
+router.post('/quizzes', 			sessionController.loginRequired, quizController.create); // Crear nuevo quiz (post)
+router.get('/quizzes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizController.edit);
+router.put('/quizzes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.update); // Actualizar un quiz (put)
+router.delete('/quizzes/:quizId(\\d+)', 	sessionController.loginRequired, quizController.destroy); // Borrar un quiz (delete)
 
-router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizzes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired, commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments',    sessionController.loginRequired, commentController.create);
 
 
 //Definición de ruta de author
